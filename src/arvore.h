b0dfc1ee100c065e5de4289ad0ea54a5f2f9ddde
BrunoGraveto <alcantarasantosbruno@gmail.com>
2025-06-30 14:45:35 -0300

    base codigo de arvore adicionado

--- src/arvore.h
+++ src/arvore.h
@@ -1,6 +1,19 @@
 #ifndef ARVORE_H
 #define ARVORE_H
 
+typedef struct NO* ArvBin;
 
+ArvBin* criarArvore();
+void liberarArvore(ArvBin *raiz);
+int inserirArvore(ArvBin* raiz, int valor);
+int removerArvore(ArvBin *raiz, int valor);
+struct NO* removerNoAtual(struct NO* atual);
+int arvoreVazia(ArvBin *raiz);
+int alturaArvore(ArvBin *raiz);
+int totalNosArvore(ArvBin *raiz);
+int consultarArvore(ArvBin *raiz, int valor);
+void preOrdemArvore(ArvBin *raiz);
+void emOrdemArvore(ArvBin *raiz);
+void posOrdemArvore(ArvBin *raiz);
 
 #endif
