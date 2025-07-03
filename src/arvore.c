3e0495671736f8e7e185ff94239997a8679d7264
BrunoGraveto <alcantarasantosbruno@gmail.com>
2025-07-03 14:14:07 -0300

    adicionado nova variavel ao struct

--- src/arvore.c
+++ src/arvore.c
@@ -6,19 +6,20 @@ struct NO {
     char* caminhoArquivo;
     char* nomeArquivo;
     char extensaoArquivo; 
-    struct NO *esq;
-    struct NO *dir;
+    struct NO *esq;  // Valor Menor
+    struct NO *cent; // Valor Igual
+    struct NO *dir;  // Valor Maior
 };
 
-Arvore* criarArvore(){
+Arvore* criarArvore() {
     Arvore* raiz = (Arvore*) malloc(sizeof(Arvore));
-    if(raiz != NULL)
+    if (raiz != NULL)
         *raiz = NULL;
     return raiz;
 }
 
-void liberarNO(struct NO* no){
-    if(no == NULL)
+void liberarNO(struct NO* no) {
+    if (no == NULL)
         return;
     liberarNO(no->esq);
     liberarNO(no->dir);
@@ -26,42 +27,41 @@ void liberarNO(struct NO* no){
     no = NULL;
 }
 
-void liberarArvore(Arvore* raiz){
-    if(raiz == NULL)
+void liberarArvore(Arvore* raiz) {
+    if (raiz == NULL)
         return;
     liberarNO(*raiz);
     free(raiz);
 }
 
-int inserirArvore(Arvore* raiz, int valor){
-    if(raiz == NULL)
+int inserirArvore(Arvore* raiz, int valor) {
+    if (raiz == NULL)
         return 0;
     struct NO* novo;
     novo = (struct NO*) malloc(sizeof(struct NO));
-    if(novo == NULL)
+    if (novo == NULL)
         return 0;
     novo->caminhoArquivo = valor;
     novo->dir = NULL;
     novo->esq = NULL;
 
-    if(*raiz == NULL)
+    if (*raiz == NULL)
         *raiz = novo;
-    else{
+    else {
         struct NO* atual = *raiz;
         struct NO* ant = NULL;
-        while(atual != NULL){
+        while (atual != NULL) {
             ant = atual;
-            if(valor == atual->caminhoArquivo){
+            if (valor == atual->caminhoArquivo) {
                 free(novo);
                 return 0;
             }
-
-            if(valor > atual->caminhoArquivo)
+            if (valor > atual->caminhoArquivo)
                 atual = atual->dir;
             else
                 atual = atual->esq;
         }
-        if(valor > ant->caminhoArquivo)
+        if (valor > ant->caminhoArquivo)
             ant->dir = novo;
         else
             ant->esq = novo;
@@ -69,17 +69,17 @@ int inserirArvore(Arvore* raiz, int valor){
     return 1;
 }
 
-int removerArvore(Arvore *raiz, int valor){
-    if(raiz == NULL)
+int removerArvore(Arvore *raiz, int valor) {
+    if (raiz == NULL)
         return 0;
     struct NO* ant = NULL;
     struct NO* atual = *raiz;
-    while(atual != NULL){
-        if(valor == atual->caminhoArquivo){
-            if(atual == *raiz)
+    while (atual != NULL) {
+        if (valor == atual->caminhoArquivo) {
+            if (atual == *raiz)
                 *raiz = removerNoAtual(atual);
-            else{
-                if(ant->dir == atual)
+            else {
+                if (ant->dir == atual)
                     ant->dir = removerNoAtual(atual);
                 else
                     ant->esq = removerNoAtual(atual);
@@ -87,7 +87,7 @@ int removerArvore(Arvore *raiz, int valor){
             return 1;
         }
         ant = atual;
-        if(valor > atual->caminhoArquivo)
+        if (valor > atual->caminhoArquivo)
             atual = atual->dir;
         else
             atual = atual->esq;
@@ -95,28 +95,28 @@ int removerArvore(Arvore *raiz, int valor){
     return 0;
 }
 
-int arvoreVazia(Arvore *raiz){
-    if(raiz == NULL)
+int arvoreVazia(Arvore *raiz) {
+    if (raiz == NULL)
         return 1;
-    if(*raiz == NULL)
+    if (*raiz == NULL)
         return 1;
     return 0;
 }
 
 struct NO* removerNoAtual(struct NO* atual) {
     struct NO *no1, *no2;
-    if(atual->esq == NULL){
+    if (atual->esq == NULL) {
         no2 = atual->dir;
         free(atual);
         return no2;
     }
     no1 = atual;
     no2 = atual->esq;
-    while(no2->dir != NULL){
+    while (no2->dir != NULL) {
         no1 = no2;
         no2 = no2->dir;
     }
-    if(no1 != atual){
+    if (no1 != atual) {
         no1->dir = no2->esq;
         no2->esq = atual->esq;
     }
@@ -125,7 +125,7 @@ struct NO* removerNoAtual(struct NO* atual) {
     return no2;
 }
 
-int alturaArvore(Arvore *raiz){
+int alturaArvore(Arvore *raiz) {
     if (raiz == NULL)
         return 0;
     if (*raiz == NULL)
@@ -135,41 +135,41 @@ int alturaArvore(Arvore *raiz){
     if (alt_esq > alt_dir)
         return (alt_esq + 1);
     else
-        return(alt_dir + 1);
+        return (alt_dir + 1);
 }
 
-void preOrdemArvore(Arvore *raiz){
-    if(raiz == NULL)
+void preOrdemArvore(Arvore *raiz) {
+    if (raiz == NULL)
         return;
-    if(*raiz != NULL){
-        printf("%d\n",(*raiz)->caminhoArquivo);
+    if (*raiz != NULL) {
+        printf("%d\n", (*raiz)->caminhoArquivo);
         preOrdemArvore(&((*raiz)->esq));
         preOrdemArvore(&((*raiz)->dir));
     }
 }
 
 void emOrdemArvore(Arvore *raiz) {
-    if(raiz == NULL)
+    if (raiz == NULL)
         return;
-    if(*raiz != NULL){
+    if (*raiz != NULL) {
         emOrdemArvore(&((*raiz)->esq));
-        printf("%d\n",(*raiz)->caminhoArquivo);
+        printf("%d\n", (*raiz)->caminhoArquivo);
         emOrdemArvore(&((*raiz)->dir));
     }
 }
 
 void posOrdemArvore(Arvore *raiz) {
-    if(raiz == NULL)
+    if (raiz == NULL)
         return;
-    if(*raiz != NULL){
+    if (*raiz != NULL) {
         posOrdemArvore(&((*raiz)->dir));
         posOrdemArvore(&((*raiz)->esq));
-        printf("%d\n",(*raiz)->caminhoArquivo);
+        printf("%d\n", (*raiz)->caminhoArquivo);
     }
 }
 
 int totalNosArvore(Arvore *raiz) {
-    if(raiz == NULL || *raiz == NULL)
+    if (raiz == NULL || *raiz == NULL)
         return 0;
     return 1 + totalNosArvore(&((*raiz)->esq)) + totalNosArvore(&((*raiz)->dir));
 }
@@ -192,18 +192,18 @@ int consultarArvore(Arvore *raiz, int valor) {
 
 struct NO* Remove_ArvoreAtual(struct NO* atual) {
     struct NO *no1, *no2;
-    if(atual->esq == NULL){
+    if (atual->esq == NULL) {
         no2 = atual->dir;
         free(atual);
         return no2;
     }
     no1 = atual;
     no2 = atual->esq;
-    while(no2->dir != NULL){
+    while (no2->dir != NULL) {
         no1 = no2;
         no2 = no2->dir;
     }
-    if(no1 != atual){
+    if (no1 != atual) {
         no1->dir = no2->esq;
         no2->esq = atual->esq;
     }
@@ -242,6 +242,7 @@ int rm(Arvore* raiz, char* diretorio) {
     Lista todos os componentes dentro da pasta atual.
 */
 int list(Arvore* raiz, char* diretorio) {
+
     return 0;
 }
 
