bb9e683524b63e06415095fd2c8c7e5ead45aa38
BrunoGraveto <alcantarasantosbruno@gmail.com>
2025-07-05 21:37:22 -0300

    feito pull(sem ser pull) do caio

--- src/arvore.h
+++ src/arvore.h
@@ -1,6 +1,9 @@
 #ifndef ARVORE_H
 #define ARVORE_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 // Struct Nó
 typedef struct NO {
     char *caminho;
@@ -18,32 +21,27 @@ typedef struct NO* Arvore;
     Funções Internas:
 */  
 Arvore* criarArvore();
-int uploadArvore(Arvore* raiz, char* caminhoArquivo);
-void removeRec(NO* no);
-NO* searchPorCaminho(Arvore* raiz, char* caminhoCompleto);
-NO* buscarPai(Arvore* raiz, char* caminhoCompleto);
-void inserirNo(Arvore* raiz, NO* pai, NO* novoNo);
+int uploadArvore(Arvore* raiz, char* caminho);
+void removeRec (NO* no);//função auxiliar para remoção de pastas e arquivos
+void liberarArvore(Arvore* raiz); //Libera a memória alocada para a árvore
 
-/*
-    Funções de Usuário:
-*/  
-Arvore cd(NO* atual, char* diretorio);
-char* search(Arvore* raiz, char* arg);
-int rm(Arvore* raiz, char* diretorio);
+// Funções de usuário
+NO* cd(NO* atual, char* diretorio);
+char* search(Arvore* raiz, char* arg); //busca  um  arquivo  ou  pasta  pelo  seu  nome  “arg”  e  informa  a sua localização
+int rm(Arvore* raiz, char* diretorio); //Remove uma pasta e seus arquivos
 int list(Arvore* raiz);
-int mkdir(Arvore* raiz, char* caminhoCompleto);
-int exitPrograma(Arvore* raiz);
+int mkdir_(Arvore* raiz, char* arg);
+void terminal(Arvore* raiz); //Função que inicia o loop terminal
+void help(); //Exibe o menu de ajuda
+int exitPrograma(Arvore* raiz); //Sai do terminal e libera a memória da árvore
+int clear(); //Limpa a tela do terminal
 
-/*
-    Funções Extras:
-*/  
+
+//Funções Extras
+int rename_nome(Arvore* raiz, char* nome_antigo, char* nome_novo); //Renomeia um arquivo ou diretório
+int echo(char* mensagem); //Exibe a mensagem especificada
+int listAll(Arvore* raiz, char* diretorio);
 int mkarq(Arvore* raiz, char* caminhoCompleto);
-
-/*
-    Funções do Terminal:
-*/  
-void terminal(Arvore* raiz);
-void help();
-void liberarArvore(Arvore* raiz); 
+NO* searchPorCaminho(Arvore* raiz, char* caminhoCompleto);
 
 #endif
